3f1bddafa2d1e2e6d52bb5148b7c906fb7ed9fa9
Metalnickk <metalnick2005@gmail.com>
2021-12-24 18:02:06 +0200

    Update.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        public int Value = 0;
+        static void Main(string[] args)
+        {
+            var generator = PasswordGenerator.Singleton;
+
+            var passwords = generator.GeneratePasswords(8, 5);
+
+        }
+    }
+}
