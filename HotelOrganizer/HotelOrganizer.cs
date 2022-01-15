fb0108770b03c95fe57446ce08d58b049c2b9e71
Metalnick <91690260+Metalnickk@users.noreply.github.com>
2022-01-15 19:43:14 +0200

    Update.

---
+++ HotelOrganizer/HotelOrganizer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelOrganizer
+{
+    public class Room
+    {
+        public string CustomerName { get; set; }
+        
+        public int ReservationPassword { get; set; }
+
+        public int Arrival { get; set; }
+
+        public int DistributionDays { get; set; }
+
+        public int People { get; set; }
+
+    }
+
+    public class Reservation
+    {
+        public int RoomPassowrd { get; set; }
+
+        public int MaxPeople { get; set; }
+
+    }
+
+
+}
