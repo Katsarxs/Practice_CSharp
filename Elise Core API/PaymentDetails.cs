98a122ba73f86ecb919e7f4564214f873d114cdc
Metalnick <91690260+Metalnickk@users.noreply.github.com>
2022-03-19 10:23:15 +0200

    Elise Update.

--- Elise Core API/PaymentDetails.cs
+++ Elise Core API/PaymentDetails.cs
@@ -54,6 +54,79 @@ namespace Elise_Core_API
         [JsonProperty("paymentMethods")]
         public PaymentMethod PaymentMethods { get; set; }
 
-        //paymentDate
+        /// <summary>
+        /// Payment Date
+        /// </summary>
+
+        [JsonProperty("paymentDate")]
+        [JsonConverter(typeof(StringToNullableDateTimeJsonConverter))]
+        public DateTime? PaymentDate { get; set;}
+
+        /// <summary>
+        /// Payment Reference Id
+        /// </summary>
+
+        [JsonProperty("paymentReferenceID")]
+        public string? PaymentReferenceID { get; set; }
+
+        /// <summary>
+        /// Paying Company Vat Number
+        /// </summary>
+
+        [JsonProperty("payingCompanyVATNumber")]
+        public string? PayingCompanyVATNumber { get; set; }
+
+        /// <summary>
+        /// Other Payment Details
+        /// </summary>
+
+        [JsonProperty("otherPaymentDetails")]
+        public string? OtherPaymentDetails { get; set; }
+
+        [JsonProperty("counterpartCurrency")]
+        public string? CounterpartCurrency { get; set; }
+
+        /// <summary>
+        /// Exchange Currency Rate
+        /// </summary>
+
+        [JsonProperty("exchangeCurrencyRate")]
+        public decimal? ExchangeCurrencyRate { get; set; }
+
+        /// <summary>
+        /// Counter Part Currency Rate
+        /// </summary>
+
+        [JsonProperty("counterpartCurrencyRate")]
+        public decimal? CounterpartCurrencyRate { get; set; }
+
+        /// <summary>
+        /// Local Currency Value
+        /// </summary>
+
+        [JsonProperty("localCurrencyValue")]
+        public decimal? LocalCurrencyValue { get; set; }
+
+        /// <summary>
+        /// Counter Part Currency Value
+        /// </summary>
+
+        [JsonProperty("counterpartCurrencyValue")]
+        public decimal? CounterpartCurrencyValue { get; set; }
+
+        /// <summary>
+        /// Exchange Currency
+        /// </summary>
+
+        [JsonProperty("exchangeCurrency")]
+        public string? ExchangeCurrency { get; set; }
+
+        /// <summary>
+        /// Counter Part New Balance
+        /// </summary>
+
+        [JsonProperty("counterpartNewBalance")]
+        public decimal? CounterpartNewBalance { get; set; }
+
     }
 }
